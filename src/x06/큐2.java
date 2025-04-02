51f2a2852a4abacd18c27f6ef0e158763ecc45da
gojungsu <gjs4565@naver.com>
2025-04-02 21:04:09 +0900

    18258 - 큐2

---
+++ src/x06/큐2.java
@@ -0,0 +1,61 @@
+package x06;    //18258 - 큐2
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class 큐2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+
+            switch (s[0]) {
+                case "push":
+                    q.offer(Integer.parseInt(s[1]));
+                    break;
+                case "front":
+                    if (q.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(q.peek()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (q.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(q.peekLast()).append("\n");
+                    }
+                    break;
+                case "pop":
+                    if (q.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    }else{
+                        sb.append(q.pop()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
+                    if (q.isEmpty()) {
+                        sb.append(1).append("\n");
+                    }else{
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
+
