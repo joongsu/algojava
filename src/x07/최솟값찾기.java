6a255e9e18fcd6d12694f060ea46245ea969776c
gojungsu <gjs4565@naver.com>
2025-04-04 18:53:50 +0900

    11003 - 최솟값 찾기

---
+++ src/x07/최솟값찾기.java
@@ -0,0 +1,34 @@
+package x07;    // 11003 - 최솟값찾기
+
+import java.io.*;
+import java.util.*;
+
+public class 최솟값찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        Deque<int[]> deq = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            int v = Integer.parseInt(st.nextToken());
+
+            while(!deq.isEmpty() && deq.peekLast()[0] > v) deq.pollLast();
+
+            deq.offer(new int[]{v, i});
+            if (deq.peek()[1] < i - L + 1) {
+                deq.pollFirst();
+            }
+
+            bw.write(deq.peek()[0] + " ");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
