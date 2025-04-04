a7d5ade7bc777638bae2bb278a34a7ebb1b63a39
gojungsu <gjs4565@naver.com>
2025-04-04 19:29:00 +0900

    4949 - 균형잡힌세상

---
+++ src/x08/균형잡힌세상.java
@@ -0,0 +1,49 @@
+package x08;    //4949 - 균형잡힌 세상
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 균형잡힌세상 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String s = br.readLine();
+            if(s.equals(".")) break;
+
+            Stack<Character> stack = new Stack<>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if(c == '(' || c == '['){
+                    stack.push(c);
+                } else if (c == ')') {
+                    if(stack.isEmpty()){
+                        stack.push(c);
+                        break;
+                    }
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    }else break;
+                } else if (c == ']') {
+                    if(stack.isEmpty()){
+                        stack.push(c);
+                        break;
+                    }
+
+                    if (stack.peek() == '[') {
+                        stack.pop();
+                    }else break;
+                }
+            }
+            if(stack.isEmpty()) sb.append("yes").append('\n');
+            else sb.append("no").append('\n');
+        }
+        System.out.print(sb);
+
+    }
+}
