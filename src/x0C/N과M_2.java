7b8ab11587292fcae7843945feaf1396e98901a1
gojungsu <gjs4565@naver.com>
2025-04-29 20:34:59 +0900

    5650 - N 과 M(2)

--- src/x0C/N과M_2.java
+++ src/x0C/N과M_2.java
@@ -1,7 +1,37 @@
-package x0C;
+package x0C;    // 15650 - N 과 M (2)
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N과M_2 {
-    public static void main(String[] args) {
 
+    static int n,m;
+    static int[] arr = new int[10];
+
+    public static void func(int cur) {
+        if (cur == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        int st = 1;
+        if(cur != 0) st = arr[cur-1] + 1;
+        for (int i = st; i <= n; i++) {
+            arr[cur] = i;
+            func(cur+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        func(0);
     }
 }
