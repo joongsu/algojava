a9c69f683774f73f5770517e57ec6d6a09322902
gojungsu <gjs4565@naver.com>
2025-08-13 15:19:03 +0900

    11779 - 최소비용 구하기 2_2nd

--- src/x1D/최소비용구하기2_2nd.java
+++ src/x1D/최소비용구하기2_2nd.java
@@ -1,90 +1,83 @@
 package x1D;    // 11779 - 최소비용 구하기 2
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 import java.util.*;
 
-
-public class 최소비용구하기2 {
-    static int V, E;
+public class 최소비용구하기2_2nd {
+    static int V, M;
     static List<List<int[]>> adj = new ArrayList<>();
-    static int start, dest;
+    static int s, e;
     static int[] d = new int[1005];
     static int[] pre = new int[1005];
-    static final int INF = Integer.MAX_VALUE;
-
+    static int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         V = Integer.parseInt(br.readLine());
-        E = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        M = Integer.parseInt(br.readLine());
 
         for (int i = 0; i <= V; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            adj.get(u).add(new int[]{w,v});
+            adj.get(u).add(new int[]{w, v});
         }
+
         st = new StringTokenizer(br.readLine());
+        s = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
 
-        start = Integer.parseInt(st.nextToken());
-        dest = Integer.parseInt(st.nextToken());
+        Arrays.fill(d, 0, V + 1, INF);
+        Arrays.fill(pre, 0, V + 1, -1);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(a -> a[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
+        d[s] = 0;
+        pq.offer(new int[]{d[s], s});
 
-        Arrays.fill(d, 0, V +1, INF);
-        d[start] = 0;
-        pq.add(new int[]{d[start],start});
-
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             int[] cur = pq.poll();
 
             if(d[cur[1]] != cur[0]) continue;
 
-            for(int[] nxt : adj.get(cur[1])){
+            for (int[] nxt : adj.get(cur[1])) {
                 if(d[nxt[1]] <= d[cur[1]] + nxt[0]) continue;
                 d[nxt[1]] = d[cur[1]] + nxt[0];
-                pq.offer(new int[]{d[nxt[1]],nxt[1]});
+                pq.offer(new int[]{d[nxt[1]], nxt[1]});
                 pre[nxt[1]] = cur[1];
             }
         }
 
-        sb.append(d[dest]).append("\n");
+        sb.append(d[e]).append("\n");
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(e);
+
         List<Integer> path = new ArrayList<>();
-
-        int cur = dest;
-
-        while(cur != start){
-            path.add(cur);
-            cur = pre[cur];
+        int v = e;
+        while (v != -1) {
+            path.add(v);
+            v = pre[v];
         }
-        path.add(cur);
-
         Collections.reverse(path);
 
         sb.append(path.size()).append("\n");
 
-        for(int i : path){
-            sb.append(i).append(" ");
+        for (int p : path) {
+            sb.append(p).append(" ");
         }
-        sb.append("\n");
-
         System.out.println(sb);
+
 
     }
 }
