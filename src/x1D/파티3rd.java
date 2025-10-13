0994126ea1411f4abab009bb87c9a882ad4b9b55
gojungsu <gjs4565@naver.com>
2025-10-13 16:00:39 +0900

    파티 - 1238 (역방향 - 3번째 풀이)

--- src/x1D/파티3rd.java
+++ src/x1D/파티3rd.java
@@ -1,16 +1,15 @@
-package x1D;
+package x1D;    // 파티 - 1238 (역방향)
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class 파티2nd {
+public class 파티3rd {
     static int V, E, X;
     static List<List<int[]>> adj = new ArrayList<>();
-    static int max = Integer.MIN_VALUE;
-    static final int INF = 1000009;
-
+    static List<List<int[]>> adjRev = new ArrayList<>();
+    static final int INF = 10000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,34 +20,39 @@ public class 파티2nd {
 
         for (int i = 0; i <= V; i++) {
             adj.add(new ArrayList<>());
+            adjRev.add(new ArrayList<>());
         }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
             adj.get(u).add(new int[]{w, v});
+            adjRev.get(v).add(new int[]{w, u});
         }
 
+        int[] distFromX = dij(adj, X);
+        int[] distToX = dij(adjRev, X);
+
+        int ans = 0;
         for (int i = 1; i <= V; i++) {
-            max = Math.max(max, dij(i, X) + dij(X, i));
+            if (distToX[i] != INF && distFromX[i] != INF) {
+                ans = Math.max(ans, distToX[i] + distFromX[i]);
+            }
         }
-
-        System.out.println(max);
+        System.out.println(ans);
     }
 
-    static int dij(int s, int e) {
+    static int[] dij(List<List<int[]>> graph, int start) {
         int[] d = new int[V + 1];
-        Arrays.fill(d, 0, V + 1, INF);
+        Arrays.fill(d, INF);
 
+        d[start] = 0;
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
-        d[s] = 0;
-
-        pq.offer(new int[]{d[s], s});
+        pq.offer(new int[]{d[start], start});
 
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
@@ -57,14 +61,15 @@ public class 파티2nd {
 
             if(cw != d[cx]) continue;
 
-            for (int[] nxt : adj.get(cx)) {
-                if (d[nxt[1]] > cw + nxt[0]) {
-                    d[nxt[1]] = cw + nxt[0];
-                    pq.offer(new int[]{d[nxt[1]], nxt[1]});
+            for (int[] nxt : graph.get(cx)) {
+                int nx = nxt[1];
+                int nw = nxt[0];
+                if (nw + d[cx] < d[nx]) {
+                    d[nx] = nw + d[cx];
+                    pq.offer(new int[]{d[nx], nx});
                 }
             }
         }
-
-        return d[e];
+        return d;
     }
 }
